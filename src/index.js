bf86530ad773b7ec212df1af0bba84ab623b4e89
Yazid Dau <yazid113@gmail.com>
2022-10-25 19:15:13 -0500

    Add db file

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+
+const taskRouter = require("./routes/task.routes");
+
+app.use(taskRouter);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
